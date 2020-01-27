f85ba595f56e48433aa93b83ff801ebd8b171798
Marie <mvperignon@gmail.com>
2020-01-27 16:31:20 +0100

    Mise à jour 2701

--- Views/View.php
+++ Views/View.php
@@ -1,1 +1,18 @@
 <?php
+
+class View
+{
+    protected $data;
+
+    function render($template = 'template'){
+        ob_start();
+        require $template.".php";
+        $str = ob_get_contents();
+        ob_end_clean();
+        return $str;
+    }
+
+    function setVar($key, $val);
+    $this->data[$key] = $val;
+
+}
