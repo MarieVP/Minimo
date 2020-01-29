f43885e35480155ac5ad2459c1578e1fbcc6a832
Marie <mvperignon@gmail.com>
2020-01-29 09:39:21 +0100

    création de Page.php

---
+++ Models/Page.php
@@ -0,0 +1,9 @@
+<?php
+
+class Page extends Post
+{
+    function __construct($values){
+        $this->title = isset($values['post_title'])?$values['post_title']: null;
+        $this->content = isset($values['post_content'])?$values['post_content']:null;
+    }
+}
